72b278178995ffba2d43ada38459ac325dc8d60a
Lakshay Garg <96138382+lakshayslayer@users.noreply.github.com>
2023-09-29 14:16:51 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -12,12 +12,19 @@ const firebaseConfig = {
   // messagingSenderId: "868749012732",
 
 
-  apiKey: "AIzaSyCP8p3ASRz6K3OWwTm5VAdwujwbMaM4Fs8",
-  authDomain: "lakshay-4e53f.firebaseapp.com",
-  projectId: "lakshay-4e53f",
-  storageBucket: "lakshay-4e53f.appspot.com",
-  messagingSenderId: "829532256758",
-  appId: "1:829532256758:web:838bc92a51b14200316fd7"
+  // apiKey: "AIzaSyCP8p3ASRz6K3OWwTm5VAdwujwbMaM4Fs8",
+  // authDomain: "lakshay-4e53f.firebaseapp.com",
+  // projectId: "lakshay-4e53f",
+  // storageBucket: "lakshay-4e53f.appspot.com",
+  // messagingSenderId: "829532256758",
+  // appId: "1:829532256758:web:838bc92a51b14200316fd7"
+
+  apiKey: "AIzaSyBWPq-8jUil-4vrGkJy2MLD7MA6Xfrv-qQ",
+  authDomain: "lakshay-a76f7.firebaseapp.com",
+  projectId: "lakshay-a76f7",
+  storageBucket: "lakshay-a76f7.appspot.com",
+  messagingSenderId: "901217934631",
+  appId: "1:901217934631:web:67b7f98656fa319ed79d02"
 
 
 
